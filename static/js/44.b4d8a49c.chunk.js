f6b9661c718fed322aa7f2cc383a6ced2b2c8577
ak <tshakh@e126.com>
2022-05-19 19:43:40 +0800

    Updates

---
+++ static/js/44.b4d8a49c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/widgets"]=this["webpackJsonp@uniswap/widgets"]||[]).push([[44],{1230:function(s,i){}}]);
+//# sourceMappingURL=44.b4d8a49c.chunk.js.map
---
+++ static/js/8.7c39763f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/widgets"]=this["webpackJsonp@uniswap/widgets"]||[]).push([[8],{1262:function(n,i){},1271:function(n,i){},1273:function(n,i){}}]);
+//# sourceMappingURL=8.7c39763f.chunk.js.map
